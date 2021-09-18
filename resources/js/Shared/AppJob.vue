c935d509888ffd0240577c071c519f1e08e1380b
George BOtsaris <georgeb@blenddigital.com>
2021-09-18 16:59:08 +0300

    Add job template

--- resources/js/Shared/AppJob.vue
+++ resources/js/Shared/AppJob.vue
@@ -4,22 +4,23 @@
             <div class="col-2 col-xl-1 text-center">
                 <img v-if="job.company_logo" :src="storageUrl + 'companies/' + job.company_logo" class="img-fluid job__logo" />
                 <div v-else class="job__logoAlternative">{{ job.company }}</div>
+                <div class="row mt-2">
+                    <div class="col-12 job__company">{{ job.company }}</div>
+                </div>
             </div>
             <div class="col-8 col-xl-9">
+                <div class="row">
+                    <div class="col-12 job__date">{{ job.formated_date }}</div>
+                </div>
                 <div class="row">
                     <div class="col-12">
                         <h3>{{ job.title }}</h3>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12 job__date">{{ job.formated_date }}</div>
-                </div>
-                <div class="row">
-                    <div class="col-12 job__company">{{ job.company }}</div>
-                </div>
                 <div class="row" v-if="job.location">
                     <div class="col-12 job__source"><i class="far fa-globe-americas"></i> {{ job.formated_location }}</div>
                 </div>
+
                 <!--<div class="row">
                     <div class="col-12 job__source">{{ job.source }}</div>
                 </div>-->
