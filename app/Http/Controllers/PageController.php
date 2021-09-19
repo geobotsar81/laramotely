520816b6047d162e9c83bb2455567fee626e1ae0
George BOtsaris <georgeb@blenddigital.com>
2021-09-19 17:41:48 +0300

    Fix responsive version

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -25,7 +25,7 @@ class PageController extends Controller
         $page=getPageFromSlug("/");
         $data=[];
 
-        $jobs=Job::paginate(10);
+        $jobs=Job::orderBy('posted_date','desc')->paginate(8);
 
         if(!empty($page)){
             $data=['title' => $page->title,'description' => $page->meta_description,'url' =>route('home.show')];
