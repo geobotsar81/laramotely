be4e880c226cafba9488a8b2380af98f7e9fe259
George BOtsaris <georgeb@blenddigital.com>
2021-09-25 18:32:44 +0300

    Merge branch 'home-changes' of github.com:geobotsar81/laramotely into home-changes

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,7 +1,7 @@
 <?xml version="1.0"  encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom"> 
 <channel>
-<title>>laramotely.com Feed</title>
+<title>laramotely.com Feed</title>
 <link>https://www.laramotely.com/feed</link>
 <atom:link href="https://www.laramotely.com/feed" rel="self" type="application/rss+xml" />
 <description>Latest JObs laramotely.com</description>
