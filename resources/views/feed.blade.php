92a31d010ec6e20fccb9a6abe7896f14a2162644
George BOtsaris <georgeb@blenddigital.com>
2021-09-25 18:23:38 +0300

    Merge branch 'home-changes' of github.com:geobotsar81/laramotely into home-changes

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -8,11 +8,11 @@
     <updated>{{ $jobs->first()->updated_at->format('D, d M Y H:i:s +0000') }}</updated>
     @foreach ($jobs as $job)
     <entry>
-        <title>@php echo $job->company." is looking for a ".$job->title.". Location: ".$job->location.". Read more at ".$job->url; @endphp</title>
+        <title><![CDATA[@php echo $job->company." is looking for a ".$job->title.". Location: ".$job->location.". Read more at ".$job->url; @endphp]]></title>
         <link>{{ route('job.show',$job->id) }}</link>
         <id>{{$job->id }}</id>
         <author>
-            <name> laramotely.com</name>
+            <name> <![CDATA[laramotely.com]]></name>
         </author>
         <date>{{ $job->posted_date }}</date>
     </entry>
