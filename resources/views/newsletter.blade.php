3929937ebee88bf18ac7e98857f78d9e747304e4
George BOtsaris <georgeb@blenddigital.com>
2021-10-16 13:03:38 +0300

    Add ogimages

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -3,11 +3,19 @@
 <h1>Check out today's jobs on laramotely.com</h1>
 
 @foreach($jobs as $job)
-{{ $job->title }}
 
-@component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id]) 
-View Job 
-@endcomponent 
+
+@component('mail::panel')
+<h2>{{ $job->title }}</h2>
+$job->location
+
+    @component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id]) 
+    View Job 
+    @endcomponent 
+
+@endcomponent
+
+
 
 @endforeach
 
--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,13 @@
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block">
+            @if (trim($slot) === 'Laravel')
+            <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo" />
+            @else
+            <span class="logo">
+                lara<span>m<i class="far fa-globe-americas"></i>tely</span>
+            </span>
+            @endif
+        </a>
+    </td>
 </tr>
