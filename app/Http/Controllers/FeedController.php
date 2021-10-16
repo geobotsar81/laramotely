888d14c9091b7bb932c94393a992e52e1062136d
George BOtsaris <georgeb@blenddigital.com>
2021-10-16 12:12:03 +0300

    Add ogimages

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Job;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class FeedController extends Controller
 {
     public function index()
     {
+
+        DB::enableQueryLog();
+
         $jobs = Job::laravel()->notother()->whereDate('posted_date','>=',Carbon::today())->orderBy('created_at','desc')->take(20)->get();
         if(empty($jobs)){
             $jobs = Job::laravel()->notother()->whereDate('posted_date','>=',Carbon::yesterday())->orderBy('created_at','desc')->take(20)->get();
         }
+
+        dd(DB::getQueryLog());
+        
         return response()->view('feed', [
             'jobs' => $jobs,
         ])->header('Content-Type', 'text/xml');
