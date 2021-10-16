ef9163ea876861ce896f40369299112735897439
George BOtsaris <georgeb@blenddigital.com>
2021-10-16 13:43:07 +0300

    Add ogimages

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -6,7 +6,9 @@
 
 @component('mail::panel')
 <h2>{{ $job->title }}</h2>
-
+<div class="job__date">{{ $job->formated_date }}</div>
+<div class="job__location">{{ $job->formated_location }}</div>
+<div class="job__company">by {{ $job->company }}</div>
 @component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id]) 
 View Job 
 @endcomponent 
