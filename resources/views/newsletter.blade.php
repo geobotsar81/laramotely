a4d1f849592d4bf424da436fccfcfbc99f3238b0
George BOtsaris <georgeb@blenddigital.com>
2021-10-16 13:46:15 +0300

    Add ogimages

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -5,8 +5,9 @@
 @foreach($jobs as $job)
 
 @component('mail::panel')
-<h2>{{ $job->title }}</h2>
 <div class="job__date">{{ $job->formated_date }}</div>
+<h2>{{ $job->title }}</h2>
+
 <div class="job__location">{{ $job->formated_location }}</div>
 <div class="job__company">by {{ $job->company }}</div>
 @component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id]) 
