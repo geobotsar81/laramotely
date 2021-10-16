b62b5ad1d1962b7fe31aa2a83860c4d167caae46
George BOtsaris <georgeb@blenddigital.com>
2021-10-16 12:59:13 +0300

    Add ogimages

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -1,11 +1,15 @@
 @component('mail::message')
 
-<h1>Check out today's Laravel jobs on laramotely.com</h1>
+<h1>Check out today's jobs on laramotely.com</h1>
 
 @foreach($jobs as $job)
 {{ $job->title }}
 
-@component('mail::button', ['url' => $job->id]) View Job @endcomponent @endforeach
+@component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id]) 
+View Job 
+@endcomponent 
+
+@endforeach
 
 {{ config("app.name") }}
 @endcomponent
