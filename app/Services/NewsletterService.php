4a3986cc53ca635c87c912aa48a9656e5e96875b
George BOtsaris <georgeb@blenddigital.com>
2021-10-16 18:22:12 +0300

    Add ogimages

--- app/Services/NewsletterService.php
+++ app/Services/NewsletterService.php
@@ -14,7 +14,9 @@ class NewsletterService{
         echo 'Got newsletter';
         $jobs = Job::laravel()->notother()->whereDate('created_at','>=',Carbon::yesterday())->whereDate('posted_date','>=',Carbon::yesterday())->orderBy('posted_date','desc')->take(10)->get();
         $contacts=Email::where('is_subscribed',1)->where('email','geobotsar@hotmail.com')->get();
-       
+        echo $jobs->count()." jobs";
+        echo $contacts->count()." contacts";
+        
         if(!empty($jobs)){
             if(!empty($contacts)){
                 foreach($contacts as $contact){
