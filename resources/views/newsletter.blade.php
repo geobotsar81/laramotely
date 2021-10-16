efca590c6731fec16bcae2667f4e75250d06c1c0
George BOtsaris <georgeb@blenddigital.com>
2021-10-16 13:25:47 +0300

    Add ogimages

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -1,12 +1,12 @@
 @component('mail::message')
 
-    <h1>Check out today's jobs on laramotely.com</h1>
+<h1>Check out today's jobs on laramotely.com</h1>
 
-    @foreach($jobs as $job)
+@foreach($jobs as $job)
 
-            <h2>{{ $job->title }}</h2>
+<h2>{{ $job->title }}</h2>
 
-    @endforeach
+@endforeach
 
 {{ config("app.name") }}
 @endcomponent
