318466dc52706871add393b326f3befbe0d3bc20
George BOtsaris <georgeb@blenddigital.com>
2021-10-16 17:47:31 +0300

    Add ogimages

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -2,22 +2,16 @@
 
 <h1>Check out today's jobs on laramotely.com</h1>
 
-@foreach($jobs as $job)
-
-@component('mail::panel')
+@foreach($jobs as $job) @component('mail::panel')
 <div class="job">
-<div class="job__date">{{ $job->formated_date }}</div>
-<h2>{{ $job->title }}</h2>
+    <div class="job__date">{{ $job->formated_date }}</div>
+    <h2>{{ $job->title }}</h2>
 
-<div class="job__location">{{ $job->formated_location }}</div>
-<div class="job__company">by {{ $job->company }}</div>
-@component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id]) 
-View Job 
-@endcomponent 
+    <div class="job__location">{{ $job->formated_location }}</div>
+    <div class="job__company">by {{ $job->company }}</div>
+    @component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id]) View Job @endcomponent
 </div>
-@endcomponent
-
-@endforeach
-
-<div class="job__unsubscribe"><a href="{{route('newsletter.unsubscribe',$contactLink)}}">unsubscribe</a></div>
+@endcomponent @endforeach
+<div class="job__reminder">You are receiving this email because you have subscribed to our daily jobs newsletter via our website.</div>
+<div class="job__unsubscribe"><a href="{{ route('newsletter.unsubscribe', $contactLink) }}">unsubscribe</a></div>
 @endcomponent
