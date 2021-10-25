489967adcb0a6bee472223d3a555f9ed4db53593
George BOtsaris <georgeb@blenddigital.com>
2021-10-25 18:41:40 +0300

    Add ogimages

--- resources/views/newsletter.blade.php
+++ resources/views/newsletter.blade.php
@@ -9,7 +9,7 @@
 
 <div class="job__location">{{ $job->formated_location }}</div>
 <div class="job__company">by {{ $job->company }}</div>
-@component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id]) View Job @endcomponent
+@component('mail::button', ['url' => 'https://www.laramotely.com/job/'.$job->id."?ref=nsl"]) View Job @endcomponent
 </div>
 @endcomponent @endforeach
 <div class="job__reminder">You are receiving this email because you have subscribed to our daily jobs newsletter via our website.</div>
