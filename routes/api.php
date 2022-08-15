6700d01ce97071c9eb82bb03113d69371b19ab33
George Botsaris <george@remazing.eu>
2022-08-15 13:52:00 +0300

    Add api calls for android app

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
     return $request->user();
+});
+
+//Route::middleware(["auth:sanctum", "throttle:10,1"])->group(function () {
+Route::middleware(["throttle:10,1"])->group(function () {
+    Route::post("/v1/get-jobs", [ApiController::class, "getJobs"])->name("api.jobs.get");
 });
