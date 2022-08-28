aa6527145b8431da78c64e60987538763e681a7f
George Botsaris <george@remazing.eu>
2022-08-28 08:50:37 +0300

    Add remote scope

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -34,69 +34,29 @@ class ApiController extends Controller
         $withReact = $request["withReact"];
         $strictSearch = $request["strictSearch"];
 
-        if ($onlyRemote) {
-            $jobs = Job::where(function ($query) use ($strictSearch) {
-                $remoteSearch = "remote";
-                $anywhereSearch = "anywhere";
+        $jobs = Job::where("title", "LIKE", "%{$search}%")
+            ->orWhere("location", "LIKE", "%{$search}%")
+            ->orWhere("tags", "LIKE", "%{$search}%")
+            ->orWhere("company", "LIKE", "%{$search}%")
+            ->published()
+            ->laravel()
+            ->notother();
 
-                $query
-                    ->where("title", "LIKE", "%{$remoteSearch}%")
-                    ->orWhere("location", "LIKE", "%{$remoteSearch}%")
-                    ->orWhere("tags", "LIKE", "%{$remoteSearch}%")
-                    ->orWhere("company", "LIKE", "%{$remoteSearch}%")
-                    ->orWhere("title", "LIKE", "%{$anywhereSearch}%")
-                    ->orWhere("location", "LIKE", "%{$anywhereSearch}%")
-                    ->orWhere("tags", "LIKE", "%{$anywhereSearch}%")
-                    ->orWhere("company", "LIKE", "%{$anywhereSearch}%");
-
-                if (!$strictSearch) {
-                    $query->orWhere("description", "LIKE", "%{$remoteSearch}%")->orWhere("description", "LIKE", "%{$anywhereSearch}%");
-                }
-            })
-                ->where(function ($query) use ($search, $strictSearch) {
-                    $query
-                        ->where("title", "LIKE", "%{$search}%")
-                        ->orWhere("location", "LIKE", "%{$search}%")
-                        ->orWhere("tags", "LIKE", "%{$search}%")
-                        ->orWhere("company", "LIKE", "%{$search}%");
-
-                    if (!$strictSearch) {
-                        $query->orWhere("description", "LIKE", "%{$search}%");
-                    }
-                })
-                ->published()
-                ->laravel()
-                ->notother();
-
-            if ($withVue) {
-                $jobs = $jobs->vue(!$strictSearch);
-            }
-            if ($withReact) {
-                $jobs = $jobs->react(!$strictSearch);
-            }
-
-            $jobs = $jobs->orderBy("posted_date", "desc")->paginate(25);
-        } else {
-            $jobs = Job::where("title", "LIKE", "%{$search}%")
-                ->orWhere("location", "LIKE", "%{$search}%")
-                ->orWhere("tags", "LIKE", "%{$search}%")
-                ->orWhere("company", "LIKE", "%{$search}%")
-                ->published()
-                ->laravel()
-                ->notother();
-
-            if (!$strictSearch) {
-                $jobs = $jobs->orWhere("description", "LIKE", "%{$search}%");
-            }
-            if ($withVue) {
-                $jobs = $jobs->vue(!$strictSearch);
-            }
-            if ($withReact) {
-                $jobs = $jobs->react(!$strictSearch);
-            }
-
-            $jobs = $jobs->orderBy("posted_date", "desc")->paginate(25);
+        if (!$strictSearch) {
+            $jobs = $jobs->orWhere("description", "LIKE", "%{$search}%");
         }
+        if ($withVue) {
+            $jobs = $jobs->vue(!$strictSearch);
+        }
+        if ($withReact) {
+            $jobs = $jobs->react(!$strictSearch);
+        }
+
+        if ($onlyRemote) {
+            $jobs = $jobs->react(!$strictSearch);
+        }
+
+        $jobs = $jobs->orderBy("posted_date", "desc")->paginate(25);
 
         return response()->json($jobs);
     }
