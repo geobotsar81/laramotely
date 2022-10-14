f319ab22c86702f9042fd674410bcdcf62fe442d
George Botsaris <george@remazing.eu>
2022-10-14 17:38:31 +0300

    Add Laravel News

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -19,9 +19,19 @@ class FeedController extends Controller
         $jobs = Job::laravel(false)
             ->notother()
             ->remote(false)
+            ->whereDate("posted_date", ">=", Carbon::now()->subDays(7))
             ->orderBy("created_at", "desc")
-            ->take(50)
+            ->take(20)
             ->get();
+        if (empty($jobs) || $jobs->count() == 0) {
+            $jobs = Job::laravel(false)
+                ->notother()
+                ->remote(false)
+                ->whereDate("posted_date", ">=", Carbon::now()->subDays(14))
+                ->orderBy("created_at", "desc")
+                ->take(20)
+                ->get();
+        }
 
         return response()
             ->view("feed", [
