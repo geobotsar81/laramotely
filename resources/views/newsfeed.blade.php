c0301a587def672fce6713548cc38cf9dd179724
George Botsaris <george@remazing.eu>
2022-10-14 17:41:28 +0300

    Add Laravel News

--- resources/views/newsfeed.blade.php
+++ resources/views/newsfeed.blade.php
@@ -5,16 +5,14 @@
 <link>https://www.laramotely.com/newsfeed</link>
 <description>Latest news at laramotely.com</description>
 <language>en-us</language>  
-    @foreach ($jobs as $job)
+    @foreach ($news as $article)
     <item>
-        @if($job->company)
-        <title><![CDATA[@php echo $job->company." is looking for a ".$job->title.". Location: ".$job->location; @endphp]]></title>
-        @else
-        <title><![CDATA[@php echo $job->title." needed. Location: ".$job->location; @endphp]]></title>
-        @endif
-        <link>{{ route('job.show',$job->id) }}</link>
-        <guid>{{ route('job.show',$job->id) }}</guid>
-        <pubDate>@php echo date('r', strtotime($job->posted_date)); @endphp</pubDate>
+       
+        <title><![CDATA[@php echo $article->title." - ".$article->category; @endphp]]></title>
+       
+        <link>{{ route('article.show',$article->id) }}</link>
+        <guid>{{ route('article.show',$article->id) }}</guid>
+        <pubDate>@php echo date('r', strtotime($article->posted_date)); @endphp</pubDate>
     </item>
     @endforeach
 </channel>
